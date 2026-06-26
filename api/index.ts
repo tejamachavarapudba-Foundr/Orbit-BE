c8b93b1fa5c8b38cfb47e993da034f5322808298
tejamachavarapums-afk <tejamachavarapums@gmail.com>
2026-06-26 15:12:35 +0530

    Fix Vercel deployment

--- api/index.ts
+++ api/index.ts
@@ -1,56 +1,36 @@
-import serverlessExpress from '@codegenie/serverless-express';
-import { NestFactory } from '@nestjs/core';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import express from 'express';
-import { AppModule } from 'src/app.module';
+import serverlessExpress from "@codegenie/serverless-express";
+import { NestFactory } from "@nestjs/core";
+import { ExpressAdapter } from "@nestjs/platform-express";
+import express from "express";
 
-let cachedServer: any;
+import { AppModule } from "../src/app.module";
+
+let server: any;
 
 async function bootstrap() {
-  try {
-    if (!cachedServer) {
-      console.log('Starting Nest app...');
+  if (!server) {
+    const expressApp = express();
 
-      const expressApp = express();
-      
-      const compiled = require('../dist/api/index.js');
-      module.exports = compiled.default || compiled;
-      
-      const nestApp = await NestFactory.create(
-        AppModule,
-        new ExpressAdapter(expressApp),
-      );
+    const app = await NestFactory.create(
+      AppModule,
+      new ExpressAdapter(expressApp),
+    );
 
-      console.log('Nest app created');
+    app.enableCors();
 
-      nestApp.enableCors();
-      nestApp.setGlobalPrefix('api');
+    app.setGlobalPrefix("api");
 
-      await nestApp.init();
+    await app.init();
 
-      console.log('Nest app initialized');
-
-      cachedServer = serverlessExpress({ app: expressApp });
-
-      console.log('Serverless Express initialized');
-    }
-
-    return cachedServer;
-  } catch (err) {
-    console.error('BOOTSTRAP ERROR:', err);
-    throw err;
+    server = serverlessExpress({
+      app: expressApp,
+    });
   }
+
+  return server;
 }
 
 export default async function handler(req: any, res: any) {
-  try {
-    console.log('Incoming request:', req.url);
-
-    const server = await bootstrap();
-
-    return server(req, res);
-  } catch (err) {
-    console.error('HANDLER ERROR:', err);
-    throw err;
-  }
+  const serverInstance = await bootstrap();
+  return serverInstance(req, res);
 }
